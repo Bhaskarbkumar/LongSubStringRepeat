21ccad22da97a3e9dc193186a7934ee08b5113b1
bharath <bhaskarbkumar@gmail.com>
2024-06-28 15:14:47 -0500

    first commit

---
+++ Repeat.java
@@ -0,0 +1,38 @@
+import java.util.HashSet;
+
+public class Repeat{
+
+
+public static void main(String[] args) {
+    
+      String str = "MissionImpossible";
+      System.out.println(longest(str));
+
+}
+
+private static String longest(String str) {
+    
+    HashSet<Character> set = new HashSet<>();
+    String longestOverall = "";
+    String longestTillNow ="";
+
+    for(int i=0 ; i<str.length();i++){
+        char c = str.charAt(i);
+        if(set.contains(c)){
+            longestTillNow ="";
+            set.clear();
+        }
+        set.add(c);
+        longestTillNow+=c;
+         if(longestTillNow.length()>longestOverall.length()){
+            longestOverall=longestTillNow;
+         }
+
+    }
+    return longestOverall;
+
+}
+
+}
+
+
